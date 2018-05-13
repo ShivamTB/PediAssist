b64145060baba66631bebba7c5e4ffd361ae9815
Raghav Kanwal <raghav.kanwal@gmail.com>
2018-05-13 19:25:28 +0530

    Changes

--- PediAssist/settings.py
+++ PediAssist/settings.py
@@ -87,12 +87,8 @@ WSGI_APPLICATION = 'PediAssist.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'pediassist',
-        'USER': 'postgres',
-        'PASSWORD': 'Stb10102010',
-        'HOST': '127.0.0.1',
-        'PORT': '5432'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
