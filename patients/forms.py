dd8d523e5466e32a612b91b02b2ac43472be4770
Shivam Agarwal <nakul1010@gmail.com>
2018-05-20 22:33:32 +0530

    dateTIme

--- patients/forms.py
+++ patients/forms.py
@@ -1,11 +1,17 @@
 from django import forms
 from .models import Patient, BirthHistory
 
+class DateInput(forms.DateInput):
+    input_type = 'date'
+
+
 class PatientForm(forms.ModelForm):
     class Meta:
         model = Patient
         fields = '__all__'
-
+        widgets = {
+            'dob': DateInput()
+        }
 
 class History(forms.ModelForm):
     class Meta:
